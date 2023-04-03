6cf32beaf4aaa14ca5e93b7cd825890a44135bc5
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-04-04 00:20:59 +0530

    commit

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
-import { setActiveSong, playPause } from '../redux/features/playerSlice';
+
 import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
-import { data } from 'autoprefixer';
 
 const ArtistDetails = () => {
-  const dispatch = useDispatch();
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
-  console.log(artistData?.data[0]?.views['top-songs']?.data)
-  const topSongsData = artistData?.data[0]?.views['top-songs']?.data;
-  const handlePauseClick = () => {
-    dispatch(playPause(false));
-  };
-
-  const handlePlayClick = (song, i) => {
-    dispatch(setActiveSong({ song, data, i }));
-    dispatch(playPause(true));
-  };
 
   if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
 
@@ -30,21 +18,69 @@ const ArtistDetails = () => {
     <div className="flex flex-col">
       <DetailsHeader
         artistId={artistId}
-        artistData={artistData}
+        artistData={artistData?.data[0]}
       />
 
       <RelatedSongs
-        // data={Object.values(artistData?.songs)}
-        title='Their Top Songs'
-        data={topSongsData}
+        data={artistData?.data[0].views['top-songs']?.data}
         artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
-        handlePauseClick={handlePauseClick}
-        handlePlayClick={handlePlayClick}
       />
     </div>
   );
 };
 
 export default ArtistDetails;
+
+
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+// import { setActiveSong, playPause } from '../redux/features/playerSlice';
+// import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+// import { data } from 'autoprefixer';
+
+// const ArtistDetails = () => {
+//   const dispatch = useDispatch();
+//   const { id: artistId } = useParams();
+//   const { activeSong, isPlaying } = useSelector((state) => state.player);
+//   const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
+//   console.log(artistData?.data[0]?.views['top-songs']?.data)
+//   const topSongsData = artistData?.data[0]?.views['top-songs']?.data;
+//   const handlePauseClick = () => {
+//     dispatch(playPause(false));
+//   };
+
+//   const handlePlayClick = (song, i) => {
+//     dispatch(setActiveSong({ song, data, i }));
+//     dispatch(playPause(true));
+//   };
+
+//   if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
+
+//   if (error) return <Error />;
+
+//   return (
+//     <div className="flex flex-col">
+//       <DetailsHeader
+//         artistId={artistId}
+//         artistData={artistData}
+//       />
+
+//       <RelatedSongs
+//         // data={Object.values(artistData?.songs)}
+//         title='Their Top Songs'
+//         data={topSongsData}
+//         artistId={artistId}
+//         isPlaying={isPlaying}
+//         activeSong={activeSong}
+//         handlePauseClick={handlePauseClick}
+//         handlePlayClick={handlePlayClick}
+//       />
+//     </div>
+//   );
+// };
+
+// export default ArtistDetails;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,72 +1,109 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-// For using Redux components within the project we imported these things 
 
-
-// Started Creating an API call
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            // headers.set('X-RapidAPI-Key', '1c00e11c7amsh2b0dd71641a71a3p1dde1fjsn0a37fa62479e');
-            // headers.set('X-RapidAPI-Key', 'de786d1714msh6e01f1c27e8f064p1f6abbjsn8672dcf38917');
-            headers.set('X-RapidAPI-Key', 'beb2a6e2abmshdf3c3c00de1b480p108daejsn3dee9bc9e14d');
+            headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 
             return headers;
         },
     }),
-
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/v1/charts/world' }),
-        getSongsByGenre: builder.query({ query: (genre) => `/v1/charts/genre-world?genre_code=${genre}` }),
-        getSongsByCountry: builder.query({ query: (countryCode) => `/v1/charts/country?country_code=${countryCode}` }),
-        getSongsBySearch: builder.query({ query: (searchTerm) => `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
-        getArtistDetails: builder.query({ query: (artistId) => `/v2/artists/details?artist_id=${artistId}` }),
-        getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}` }),
-        getSongRelated: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}` }),
+        getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+        getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
     }),
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+    useGetArtistDetailsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
-    useGetArtistDetailsQuery,
-    useGetSongsByCountryQuery,
-    useGetSongsByGenreQuery,
-    useGetSongsBySearchQuery,
 } = shazamCoreApi;
 
+
+
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// // For using Redux components within the project we imported these things 
+
+
+// // Started Creating an API call
+// export const shazamCoreApi = createApi({
+//     reducerPath: 'shazamCoreApi',
+//     baseQuery: fetchBaseQuery({
+//         baseUrl: 'https://shazam-core.p.rapidapi.com/',
+//         prepareHeaders: (headers) => {
+//             // headers.set('X-RapidAPI-Key', '1c00e11c7amsh2b0dd71641a71a3p1dde1fjsn0a37fa62479e');
+//             // headers.set('X-RapidAPI-Key', 'de786d1714msh6e01f1c27e8f064p1f6abbjsn8672dcf38917');
+//             headers.set('X-RapidAPI-Key', 'beb2a6e2abmshdf3c3c00de1b480p108daejsn3dee9bc9e14d');
+
+//             return headers;
+//         },
+//     }),
+
 //     endpoints: (builder) => ({
-//         getTopCharts: builder.query({ query: () => '/charts/world' }),
-//         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-//         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-//         getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+//         getTopCharts: builder.query({ query: () => '/v1/charts/world' }),
+//         getSongsByGenre: builder.query({ query: (genre) => `/v1/charts/genre-world?genre_code=${genre}` }),
+//         getSongsByCountry: builder.query({ query: (countryCode) => `/v1/charts/country?country_code=${countryCode}` }),
+//         getSongsBySearch: builder.query({ query: (searchTerm) => `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+//         getArtistDetails: builder.query({ query: (artistId) => `/v2/artists/details?artist_id=${artistId}` }),
+//         getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}` }),
+//         getSongRelated: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}` }),
 //     }),
 // });
 
-// // Note that Redux allows us to call this query of getTopCharts as a hook
 // export const {
 //     useGetTopChartsQuery,
 //     useGetSongDetailsQuery,
 //     useGetSongRelatedQuery,
 //     useGetArtistDetailsQuery,
+//     useGetSongsByCountryQuery,
+//     useGetSongsByGenreQuery,
+//     useGetSongsBySearchQuery,
 // } = shazamCoreApi;
 
-    // The procees of getting a hold of this api thing is via the help of all the things we have learnt so far using Shazam API of the Rapid API things we use include:
-    // Request: https:// (url)
-    //Headers name and values
+// //     endpoints: (builder) => ({
+// //         getTopCharts: builder.query({ query: () => '/charts/world' }),
+// //         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+// //         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+// //         getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+// //     }),
+// // });
 
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// // // Note that Redux allows us to call this query of getTopCharts as a hook
+// // export const {
+// //     useGetTopChartsQuery,
+// //     useGetSongDetailsQuery,
+// //     useGetSongRelatedQuery,
+// //     useGetArtistDetailsQuery,
+// // } = shazamCoreApi;
 
-// export const shazamCoreApi = createApi({
-//     reducerPath: 'shazamCoreApi',
-//     baseQuery: fetchBaseQuery({
-//         baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-//         prepareHeaders: (headers) => {
-//             headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+//     // The procees of getting a hold of this api thing is via the help of all the things we have learnt so far using Shazam API of the Rapid API things we use include:
+//     // Request: https:// (url)
+//     //Headers name and values
 
-//             return headers;
-//         },
-//     }),
-//     
+// // import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// // export const shazamCoreApi = createApi({
+// //     reducerPath: 'shazamCoreApi',
+// //     baseQuery: fetchBaseQuery({
+// //         baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+// //         prepareHeaders: (headers) => {
+// //             headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+
+// //             return headers;
+// //         },
+// //     }),
+// //
+
+// 
